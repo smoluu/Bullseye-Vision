14f926583883d9903960b737bf9ab01fe865003a
smolu <smolunpapugmail.com>
2024-03-07 18:36:42 +0200

    using wapedImage function

--- Backend/cameraViewTransformed.py
+++ Backend/cameraViewTransformed.py
@@ -1,9 +1,6 @@
 import jpgReceive
+import transform
 import cv2
-import numpy as np
-
-matrixL = np.load("Backend/transformation_matrix_L.npy",)
-matrixR = np.load("Backend/transformation_matrix_R.npy",)
 
 
 while True:
@@ -16,12 +13,12 @@ while True:
     srcR = cv2.flip(srcR,0)
     srcR = cv2.flip(srcR,1)
 
+    srcL = transform.warpedImage(srcL,"left")
+    srcR = transform.warpedImage(srcR,"right")
 
-    warpedL = cv2.warpPerspective(srcL, matrixL, (1600, 1200))
-    warpedR = cv2.warpPerspective(srcR, matrixR, (1600, 1200))
 
-    cv2.imshow("Transformed Image L", warpedL)
-    cv2.imshow("Transformed Image R", warpedR)
+    cv2.imshow("Transformed Image L", srcL)
+    cv2.imshow("Transformed Image R", srcR)
 
     # wait for the key and come out of the loop 
     if cv2.waitKey(1) == ord('q'):
